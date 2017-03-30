10967395a73d95883bb9e9a17a6c0295375bee35
Igor C. Batista <igor.batista@gmail.com>
2017-03-30 14:09:19 -0300

    revert commit trash

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122103648) do
+ActiveRecord::Schema.define(version: 20160221134835) do
 
   create_table "black_lists", force: :cascade do |t|
     t.string   "name"

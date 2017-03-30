52ef5bc501d2a33b9c383dfbd06965a12738d8eb
Igor C. Batista <igor.batista@gmail.com>
2017-03-30 14:04:54 -0300

    revert commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221134835) do
+ActiveRecord::Schema.define(version: 20161122103648) do
+
+  create_table "black_lists", force: :cascade do |t|
+    t.string   "name"
+    t.string   "fone"
+    t.string   "cpf"
+    t.text     "motivo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "districts", force: :cascade do |t|
     t.string "name"
